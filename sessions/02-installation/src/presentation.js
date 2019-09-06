5edb97413491bdbd7bae6b0f6ba699c761bbfd68
Tim Geisler <tg@webxcerpt.com>
2019-09-06 19:01:36 +0200

    add some empty presentation skeletons

---
+++ sessions/02-installation/src/presentation.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Deck, Heading, Image, List, ListItem, Slide } from 'spectacle';
+import createTheme from 'spectacle/lib/themes/default';
+
+require('normalize.css');
+
+const theme = createTheme(
+  {
+    primary: 'white',
+    secondary: '#5BAEC5',
+    tertiary: '#F15922',
+    quaternary: '#CECECE',
+  },
+  {
+    primary: 'avenir-lt-w01_85-heavy1475544, sans-serif',
+    secondary: 'Helvetica',
+  }
+);
+
+export default () => (
+  <Deck
+    transition={['zoom', 'slide']}
+    transitionDuration={500}
+    theme={theme}
+    contentHeight={800}
+    contentWidth={1600}
+  >
+    <Slide>
+      <Image
+        src="redi-school-logo.webp"
+        height="50px"
+      />
+      <Heading style={{margin: '50px'}} size={1} caps>
+        ReDI Munich JavaScript 2 Course
+      </Heading>
+      <Heading style={{margin: '50px'}} size={2}>
+        Installation
+      </Heading>
+      <Heading style={{margin: '50px'}} size={3}>
+        2019-09-xx
+      </Heading>
+    </Slide>
+    <Slide>
+      <Heading size={1} caps>
+        Schedule
+      </Heading>
+      <List>
+        <ListItem>Browser: Chrome</ListItem>
+        <ListItem>Editor: VSCode + Plugins</ListItem>
+        <ListItem>Node.js + npm</ListItem>
+        <ListItem>git</ListItem>
+      </List>
+    </Slide>
+  </Deck>
+);
