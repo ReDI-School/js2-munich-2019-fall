c4cc2ce98ed96db03241b3bda5d5f534efed9001
Tim Geisler <tg@webxcerpt.com>
2019-09-08 17:03:25 +0200

    add screenshots for "first react app" session

--- sessions/03-first-react-app/src/presentation.js
+++ sessions/03-first-react-app/src/presentation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Code, Deck, Heading, Image, List, ListItem, Slide, Text } from 'spectacle';
+import { Code, Deck, Heading, Image, Layout, List, ListItem, Slide, Text } from 'spectacle';
 import Terminal from "spectacle-terminal";
 import createTheme from 'spectacle/lib/themes/default';
 
@@ -63,16 +63,59 @@ export default () => (
         Creating a React App using create-react-app
       </Heading>
       <Text>Open a shell (git bash) and type <Code>npx create-react-app my-cookbook</Code></Text>
-      <Terminal title="~(bash)" output={[
-        "$ npx create-react-app my-cookbook",
+      <Terminal title="~(git bash)" output={[
+        `tg@LAPTOP MINGW64 ~/redi
+$ npx create-react-app my-cookbook`,
+`        
+Creating a new React app in C:\\Users\\tg\\redi\\my-cookbook.
+
+Installing packages. This might take a couple of minutes.
+Installing react, react-dom, and react-scripts...
+
+
+> core-js@2.6.9 postinstall C:\\Users\\tg\\redi\\my-cookbook\\node_modules\\babel-runtime\\node_modules\\core-js
+> node scripts/postinstall || echo "ignore"
+
+
+> core-js@3.1.4 postinstall C:\Users\\tg\\redi\\my-cookbook\\node_modules\\core-js
+> node scripts/postinstall || echo "ignore"
+
++ react-scripts@3.1.1
++ react-dom@16.9.0
++ react@16.9.0
+added 1444 packages from 685 contributors in 85.316s
+[+] no known vulnerabilities found [905233 packages audited]
+
+
+Initialized a git repository.
+
+Success! Created my-cookbook at C:\\Users\\tg\\redi\\my-cookbook
+Inside that directory, you can run several commands:
+
+  npm start
+    Starts the development server.
+
+  npm run build
+    Bundles the app into static files for production.
+
+  npm test
+    Starts the test runner.
+
+  npm run eject
+    Removes this tool and copies build dependencies, configuration files
+    and scripts into the app directory. If you do this, you can’t go back!
+
+We suggest that you begin by typing:
+
+  cd my-cookbook
+  npm start
+
+Happy hacking!
+
+tg@LAPTOP MINGW64 ~/redi
+$ `,
         ]}
       />
-    </Slide>
-    <Slide>
-      <Heading size={2}>
-        Resulting Folder Structure
-      </Heading>
-      <Image />
     </Slide>
     <Slide>
       <Heading size={2} fit>
@@ -80,9 +123,28 @@ export default () => (
       </Heading>
       <Text>In git bash, type <Code>cd my-cookbook</Code> to navigate to the newly created folder.</Text>
       <Text>Type <Code>npm start</Code> to start the development server.</Text>
-      <Terminal title="~(bash)" output={[
-        "$ npm start",
-        ]}
+      <Terminal title="~(git bash)" output={[
+        `tg@LAPTOP MINGW64 ~/redi
+$ cd my-cookbook`,
+`tg@LAPTOP MINGW64 ~/redi/my-cookbook (master)
+$ npm run start`,
+        `
+> my-cookbook@0.1.0 start C:\\Users\\tg\\redi\\my-cookbook
+> react-scripts start
+
+Starting the development server...
+
+Compiled successfully!
+
+You can now view my-cookbook in the browser.
+
+  Local:            http://localhost:3000/
+  On Your Network:  http://192.168.56.1:3000/
+
+Note that the development build is not optimized.
+To create a production build, use npm run build.
+`
+          ]}
       />
     </Slide>
     <Slide>
@@ -91,14 +153,15 @@ export default () => (
       </Heading>
       <Text>If no new browser tab opens, open http://localhost:3000/ in your browser.</Text>
       <Text>Hooray! You will now see your first React app in your browser:</Text>
-      <Image />
+      <Image src="browser.png"/>
     </Slide>
     <Slide>
       <Heading size={2}>
         Editing the App
       </Heading>
       <Text>Start Visual Studio Code and open the folder <Code>my-cookbook</Code>.</Text>
-      <Image />
+      <Text>Open the file <Code>src/App.js</Code>.</Text>
+      <Image src="vs-code.png"/>
     </Slide>
     <Slide>
       <Heading size={2}>
@@ -106,20 +169,27 @@ export default () => (
       </Heading>
       <Text>Open the file <Code>src/App.js</Code>, modify some text, and save it.</Text>
       <Text>The browser reloads the page and shows the modified app.</Text>
-      <Image />
+      <Layout>
+        <Image src="vs-code-change-ok.png"/>
+        <Image src="browser-change-ok.png"/>
+      </Layout>
     </Slide>
     <Slide>
       <Heading size={2} fit>
         Modifying the App - Errors
       </Heading>
       <Text>In case you make an error, an error message is shown:</Text>
-      <Image />
+      <Layout>
+        <Image src="vs-code-change-error.png"/>
+        <Image src="browser-change-error.png"/>
+      </Layout>
+      <Text>In addition, you can see the error message in the console where you entered <Code>npm run start</Code>.</Text>
     </Slide>
     <Slide>
       <Heading size={2} fit>
         What do all the Created Files Mean?
       </Heading>
-      <Image />
+      <Text>TODO: show all relevant files and add explanations</Text>
     </Slide>
     <Slide bgImage="board-928381_960_720.jpg" />
     <Slide>
