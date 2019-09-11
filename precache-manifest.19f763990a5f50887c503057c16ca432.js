1b3f9b0c1b1698ef6d41e8a8feadd4578df902ee
Tim Geisler <tg@webxcerpt.com>
2019-09-11 15:44:41 +0200

    Updates

---
+++ precache-manifest.19f763990a5f50887c503057c16ca432.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dcad3d574027b23229de",
+    "url": "/static/js/main.dcad3d57.chunk.js"
+  },
+  {
+    "revision": "7124b3dfe5627e4fc441",
+    "url": "/static/js/1.7124b3df.chunk.js"
+  },
+  {
+    "revision": "7124b3dfe5627e4fc441",
+    "url": "/static/css/1.c09ac6e4.chunk.css"
+  },
+  {
+    "revision": "43635afc26cdd2d6b944e89d12cf097e",
+    "url": "/index.html"
+  }
+];
