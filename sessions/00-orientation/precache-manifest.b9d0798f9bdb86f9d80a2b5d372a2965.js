0051d7dabfa3398d374366c5d8ab337b41c5531f
Tim Geisler <tg@webxcerpt.com>
2019-09-12 17:58:09 +0200

    Updates

---
+++ sessions/00-orientation/precache-manifest.b9d0798f9bdb86f9d80a2b5d372a2965.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "264fba1fd73e0e87540f",
+    "url": "/js2-munich-2019-fall/sessions/00-orientation/static/js/runtime~main.264fba1f.js"
+  },
+  {
+    "revision": "e9cfb5fa4ad443554e69",
+    "url": "/js2-munich-2019-fall/sessions/00-orientation/static/js/main.e9cfb5fa.chunk.js"
+  },
+  {
+    "revision": "7124b3dfe5627e4fc441",
+    "url": "/js2-munich-2019-fall/sessions/00-orientation/static/js/1.7124b3df.chunk.js"
+  },
+  {
+    "revision": "7124b3dfe5627e4fc441",
+    "url": "/js2-munich-2019-fall/sessions/00-orientation/static/css/1.c09ac6e4.chunk.css"
+  },
+  {
+    "revision": "9f06136fafc800938ad3f1a976d5fc73",
+    "url": "/js2-munich-2019-fall/sessions/00-orientation/index.html"
+  }
+];
