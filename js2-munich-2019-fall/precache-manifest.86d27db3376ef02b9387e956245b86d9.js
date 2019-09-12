7c0f1d2474ad656629415ad0cb5478fe592b1bf1
Tim Geisler <tg@webxcerpt.com>
2019-09-12 17:31:49 +0200

    Updates

---
+++ js2-munich-2019-fall/precache-manifest.86d27db3376ef02b9387e956245b86d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e9cfb5fa4ad443554e69",
+    "url": "/static/js/main.e9cfb5fa.chunk.js"
+  },
+  {
+    "revision": "7124b3dfe5627e4fc441",
+    "url": "/static/js/1.7124b3df.chunk.js"
+  },
+  {
+    "revision": "7124b3dfe5627e4fc441",
+    "url": "/static/css/1.c09ac6e4.chunk.css"
+  },
+  {
+    "revision": "e2b6ab030f3079f3ce1b6cfb12efea47",
+    "url": "/index.html"
+  }
+];
